c2040585e174726a4181269ff9340372b2c370cd
Gabi <79410834+gabrielCostache@users.noreply.github.com>
2022-11-17 20:33:54 +0200

    Add files via upload
    First upload

---
+++ ShoppingCart_01/ShoppingCart_v1/ShoppingCart_v1/Domain/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Choices;
+
+namespace ShoppingCart_v1.Domain
+{
+
+    [AsChoice]
+    
+    public static partial class Cart
+    {
+        public interface ICart { }
+        public record UnalidatedCart(IReadOnlyCollection<UnvalidatedCart> CartList):ICart;
+        public record InvalidatedCart(IReadOnlyCollection<UnvalidatedCart> CartList, string reason):ICart;
+        public record ValidatedCart(IReadOnlyCollection<ValidatedCart> CartList, string reason):ICart; 
+        public record PublishCart(IReadOnlyCollection<ValidatedCart> CartList, DateTime PublishedDate):ICart;
+    }
+
+
+}
---
+++ ShoppingCart_01/ShoppingCart_v1/ShoppingCart_v1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ShoppingCart_v1.Domain;
+using CSharp.Choices;
+//using static ShoppingCart_v1.Domain.Quantity;
+
+// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("----------------Welcome to our shopping cart----------------");
+int option = 0;
+var ContactList = new List<Contact>();
+
+Console.WriteLine("1.Add contact details and products");
+Console.WriteLine("2.Show orders");
+Console.WriteLine("\n");
+Console.WriteLine("0. Press 0 to exit");
+
+do
+{
+
+    option = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("You have selected:" + option+" option");
+    switch (option)
+    {
+        case 0: break;
+        case 1:
+            {
+                    /* must be filled */
+                break;
+            }
+
+        case 2:
+            {
+                   /* must be filled */
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("This option isn't configured yet!");
+                break;
+            }
+    }
+
+} while (option != 0);
